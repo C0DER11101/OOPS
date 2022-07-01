9dd7bc36f3a4167b7dc4b85afac3f2d892e7e650
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-07-01 20:29:42 +0530

    erronous program; cannot convert a pointer to a derived class to a pointer to a base class

---
+++ practice3.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+
+using namespace std;
+
+class A
+{
+	public:
+		int m;
+		void show();
+};
+
+class B:public A
+{
+	public:
+		int m;
+		void show();
+};
+
+void A::show()
+{
+	cout<<"\nshow() of class A:\n";
+	cout<<"Value of datamember m of class A is: "<<m<<endl;
+}
+
+void B::show()
+{
+	cout<<"\nshow() of class B:\n";
+	cout<<"Value of datamember m of class B is: "<<m<<endl;
+}
+
+
+int main(void)
+{
+	B*bptr; // pointer of derived class type!!
+
+	B b;
+	A a;
+
+	bptr=&b; // error:   cannot convert a pointer to derived class to a pointer to a base class!!
+
+	/* the rest of the below code wll not work; there's an error up here ^ */
+	bptr->m=10;
+	bptr->show();
+
+	bptr=&a;
+	bptr->m=20;
+	bptr->show();
+	return 0;
+}
